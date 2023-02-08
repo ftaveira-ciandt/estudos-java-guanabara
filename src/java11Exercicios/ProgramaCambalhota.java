137a7f9d5784a0e2b368d1494913f132e6a066d0
Francisco Taveira <ftaveira@ciandt.com>
2023-02-08 13:48:27 -0300

    [Aula 11] - Contador

---
+++ src/java11Exercicios/ProgramaCambalhota.java
@@ -0,0 +1,13 @@
+package java11Exercicios;
+
+public class ProgramaCambalhota {
+
+    public static void main(String[] args) {
+        int cc = 0;
+        while (cc<4) {
+
+            System.out.println("Cambalhota " + cc);
+            cc++;
+        }
+
+    }}
