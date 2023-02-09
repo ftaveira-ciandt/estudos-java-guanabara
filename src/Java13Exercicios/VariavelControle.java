699657e49d85313b049fdb85db243d3beed7297b
Francisco Taveira <ftaveira@ciandt.com>
2023-02-09 14:44:09 -0300

    [Aula 13] - Testa Contador Slide

---
+++ src/Java13Exercicios/VariavelControle.java
@@ -0,0 +1,17 @@
+package Java13Exercicios;
+
+public class VariavelControle {
+
+    public static void main(String[] args) {
+
+
+
+        for (int cc=0; cc <= 100; cc+=10) {
+
+            System.out.println("Cambalhota " + cc);
+
+
+        }
+    }
+
+}
