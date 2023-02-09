d6deb643f9219fba577836551e1229107c72ba81
Francisco Taveira <ftaveira@ciandt.com>
2023-02-09 16:52:23 -0300

    [Aula 14] - Vetor 05

--- src/java14Exercicios/Vetor05.java
+++ src/java14Exercicios/Vetor05.java
@@ -1,2 +1,18 @@
-package java14Exercicios;public class Vetor05 {
+package java14Exercicios;
+
+import java.util.Arrays;
+
+public class Vetor05 {
+
+    public static void main(String[] args) {
+
+        int v[] = new int[20];
+        Arrays.fill(v, 0);
+        for(int valor: v) {
+            System.out.println(valor);
+        }
+    }
+
+
+
 }
