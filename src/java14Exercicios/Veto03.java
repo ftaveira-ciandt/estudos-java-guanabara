22b40874f06edf1cc646fdf3160b4962c06ec9e9
Francisco Taveira <ftaveira@ciandt.com>
2023-02-09 16:50:32 -0300

    [Aula 14] - Vetor 03

--- src/java14Exercicios/Veto03.java
+++ src/java14Exercicios/Veto03.java
@@ -1,2 +1,15 @@
-package java14Exercicios;public class Veto03 {
+package java14Exercicios;
+
+import java.util.Arrays;
+
+public class Veto03 {
+
+    public static void main(String[] args) {
+        double v[] = {3.5, 2.75, 4.5, 9, -5.7};
+        for (double valor: v) {
+            Arrays.sort(v);
+            System.out.println("Valor na posição é " + valor);
+        }
+    }
+
 }
