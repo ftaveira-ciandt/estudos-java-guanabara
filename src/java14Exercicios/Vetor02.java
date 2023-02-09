fc16b1dd4783f9a48ed4ca041534ae986f9a4e3b
Francisco Taveira <ftaveira@ciandt.com>
2023-02-09 16:49:33 -0300

    [Aula 14] - Vetor 02

--- src/java14Exercicios/Vetor02.java
+++ src/java14Exercicios/Vetor02.java
@@ -1,2 +1,16 @@
-package java14Exercicios;public class Vetor02 {
+package java14Exercicios;
+
+public class Vetor02 {
+
+    public static void main(String[] args) {
+        String mes[] = { "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez" };
+        int dia[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        for (int c = 0; c < mes.length; c++) {
+            System.out.println("O mes " + mes[c] + " possui total de " + dia[c] + " dias.");
+
+        }
+
+    }
+
 }
