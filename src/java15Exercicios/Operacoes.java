fec92a8fbaf9558b2b1e7f607af94802efb8bea0
Francisco Taveira <ftaveira@ciandt.com>
2023-02-10 13:25:35 -0300

    [Aula 15] - Funcao Operacoes

--- src/java15Exercicios/Operacoes.java
+++ src/java15Exercicios/Operacoes.java
@@ -1,2 +1,13 @@
-package java15Exercicios;public class Operacoes {
+package java15Exercicios;
+
+public class Operacoes {
+
+    public static String contador(int i, int f) {
+        String s = " ";
+        for (int c = i; c<= f; c++) {
+            s+= c + " ";
+        }
+        return s;
+    }
+
 }
